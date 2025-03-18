91700bbbea32bfe863bd97ac39ac03fab0aabf07
boylin0 <as20757738@gmail.com>
2025-03-18 12:08:36 +0800

    it works

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,9 @@
+use tauri::{path::BaseDirectory, Manager};
+
+struct AppData {
+    apkeditor_path: String,
+}
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -32,12 +38,52 @@ fn maximize(window: tauri::Window) {
     }
 }
 
+#[tauri::command]
+fn run_apkeditor_merge(state: tauri::State<AppData>, path: String) {
+    let jar_path = &state.apkeditor_path;
+    let java_args = vec![
+        "-jar".to_string(),
+        jar_path.clone(),
+        "merge".to_string(),
+        "-i".to_string(),
+        path.clone(),
+    ];
+    print!("{:?}", java_args);
+    let output = std::process::Command::new("java")
+        .args(&java_args)
+        .output()
+        .expect("failed to execute process");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    println!("{} {}", stdout, stderr);
+    println!("{}", jar_path);
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            let resource_path = app
+                .path()
+                .resolve("resources/apkeditor.jar", BaseDirectory::Resource)?;
+            app.manage(AppData {
+                apkeditor_path: resource_path
+                    .to_string_lossy()
+                    .strip_prefix(r"\\?\")
+                    .unwrap()
+                    .to_string(),
+            });
+            Ok(())
+        })
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, close, minimize, maximize])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            close,
+            minimize,
+            maximize,
+            run_apkeditor_merge
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
-import { useState } from "react";
+import { useEffect, useRef } from "react";
 import { invoke } from "@tauri-apps/api/core";
+import { listen, UnlistenFn } from "@tauri-apps/api/event";
 
 import "@fontsource/roboto";
 import "./App.scss";
 import { open } from "@tauri-apps/plugin-dialog";
+import type { event } from "@tauri-apps/api";
 
 function WindowWrapper({ children }: { children: React.ReactNode }) {
+
+  const titlebarControlsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+
+    const handleMouseEnter = () => {
+      titlebarControlsRef.current?.classList.add("hover");
+    }
+
+    const handleMouseLeave = () => {
+      titlebarControlsRef.current?.classList.remove("hover");
+    }
+
+    titlebarControlsRef.current?.addEventListener("mouseenter", handleMouseEnter);
+    titlebarControlsRef.current?.addEventListener("mouseleave", handleMouseLeave);
+    
+    return () => {
+      titlebarControlsRef.current?.removeEventListener("mouseenter", handleMouseEnter);
+      titlebarControlsRef.current?.removeEventListener("mouseleave", handleMouseLeave);
+    };
+  }, []);
+
   return (
     <div className="window">
       <div data-tauri-drag-region className="title-bar">
-        <div className="title-bar-controls">
+        <div ref={titlebarControlsRef} className="title-bar-controls">
           <button aria-label="Close" onClick={() => invoke("close")}></button>
           <button aria-label="Minimize" onClick={() => invoke("minimize")}></button>
         </div>
@@ -25,19 +49,48 @@ function DragDropFileRegion() {
 
     //setGreetMsg(await invoke("greet", { name }));
 
+  const handleInvoke = async (path: string| null) => {
+    console.log("Invoking run_apkeditor_merge");
+    console.log(await invoke("run_apkeditor_merge", { path: path }));
+  }
+
   const handleClick = async () => {
     console.log("Opening file dialog");
-    const selectedFiles = await open({
-      multiple: true,
+    const selectedPath = await open({
+      //directory: true,
+      multiple: false,
       filters: [
         {
           name: "APK Files",
-          extensions: ["apk", "xapk", "apkm", "apks"],
+          extensions: ["xapk", "apkm", "apks"],
         },
       ],
     });
-    console.log(selectedFiles);
+    handleInvoke(selectedPath);
   };
+
+  useEffect(() => {
+    let unlisten: UnlistenFn;
+
+    const setupListener = async () => {
+      unlisten = await listen("tauri://drag-drop", (event: event.Event<{ paths: string[] }>) => {
+        const paths = event.payload.paths;
+        if (paths.length > 1 || paths.length === 0) {
+          console.error("Only one file is supported at a time");
+          return
+        }
+        handleInvoke(paths[0]);
+      });
+    };
+
+    setupListener();
+
+    return () => {
+      if (unlisten) {
+        unlisten();
+      }
+    };
+  }, []);
 
   return (
     <div
